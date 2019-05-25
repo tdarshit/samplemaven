0b675f88e313074961ef69e96ff250946d350d41
Darshit <dars10tha@gmail.com>
2019-05-25 15:26:24 -0400

    Initial push of files

---
+++ MavenProject/src/test/java/SeleniumTest/FirstMavenTest.java
@@ -0,0 +1,11 @@
+package SeleniumTest;
+
+import org.testng.annotations.Test;
+
+public class FirstMavenTest {
+	
+	@Test
+	public void testMaven() {
+		System.out.println("Testing Maven--------");
+	}
+}
---
+++ MavenProject/src/test/java/SeleniumTest/SecondMavenTest.java
@@ -0,0 +1,11 @@
+package SeleniumTest;
+
+import org.testng.annotations.Test;
+
+public class SecondMavenTest {
+	
+	@Test
+	public void testMaven() {
+		System.out.println("Testing Maven Twice----------");
+	}
+}
